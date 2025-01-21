1dcc834feb7b7600067317632ea69a8e097fca97
Akshay <akshayu718@gmail.com>
2025-01-21 21:03:50 +0530

    write code to fetch SpaceX's api data to show the upcoming launches

--- index.js
+++ index.js
@@ -5,27 +5,49 @@ import axios from 'axios';
 const app = express();
 const PORT = 3000;
 
+// Make public folder static
 app.use(express.static('public'));
 
+// NASA API Setup
 const nasaUrl = 'https://api.nasa.gov/planetary/apod';
 const nasaApiKey = 'YsekNWZ4I8spQrWPkJ8AOFaNKwVGsytlGFLCFbX1'
 
+// SpaceX url Setup
+const spaceXUrl = 'https://api.spacexdata.com/v5/launches/upcoming';
+
+// GET route that fetches data from both NASA and SpaceX APIs
 app.get('/', async (req, res)=>{
+    // fix date
     const date = req.query.date || '2025-01-20';
     try{
+        // Fetch NASA current image data first
         const response = await axios.get(`${nasaUrl}?api_key=${nasaApiKey}&date=${date}`);
         const currentApod = response.data;
         
+        // Setup for past date to get previous images
         const pastDate = new Date();
         pastDate.setDate(pastDate.getDate() - 37);
         const startDate = pastDate.toISOString().split('T')[0];
-    
+        
+        // fetch past NASA image data 
         const pastResponse = await axios.get(`${nasaUrl}?api_key=${nasaApiKey}&start_date=${startDate}&end_date=${date}`);
         const pastApods = pastResponse.data;
 
-        res.render('index.ejs', {apod : currentApod, pastApods });
+        // Fetch SpaceX data after NASA request is completed
+        const spacexResponse = await axios.get(spaceXUrl);
+        const upcomingLaunches = spacexResponse.data;
+
+        const nextLaunch = upcomingLaunches[0];
+
+        // Render NASA nad SpaceX data to the EJS template
+        res.render('index.ejs', {
+            apod : currentApod, pastApods,
+            nextLaunch, 
+            upcomingLaunches
+        });
     } catch(error) {
-        res.render("index.ejs", { apod : error.response.data });
+        console.error('Error fetching data:', error);
+        res.status(500).send('Something went wrong');
     }
 })
 
