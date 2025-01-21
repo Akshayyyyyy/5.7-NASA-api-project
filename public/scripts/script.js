ee123410106e82ca754742ec1fba5fcb4eb11ed4
Akshay <akshayu718@gmail.com>
2025-01-21 14:04:57 +0530

    write script for the fuctionality of the image gallary to enlarge image

---
+++ public/scripts/script.js
@@ -0,0 +1,15 @@
+function openModal(imageUrl, title) {
+    var modal = document.getElementById("imageModal");
+    var modalImage = document.getElementById("modalImage");
+    var caption = document.getElementById("caption");
+
+    // Set the modal image source and caption
+    modal.style.display = "block";
+    modalImage.src = imageUrl;
+    caption.innerHTML = title;
+  }
+
+  function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+  }
